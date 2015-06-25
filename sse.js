08aba726ea903e3b16661d9515d607c3dd395860
BoppreH <boppreh@gmail.com>
2015-06-25 03:18:47 +0000

    Update sse.js to mimick that from bop's

---
+++ sse.js
@@ -0,0 +1,46 @@
+"ues strict";
+
+var RAND_ALPHABET = "0123456789abcdef"
+function randId(bits) {
+    var n = bits / Math.log2(RAND_ALPHABET.length);
+    var chars = []
+    for (var i = 0; i < n; i++) {
+        var randIndex = Math.floor(Math.random() * RAND_ALPHABET.length);
+        chars.push(RAND_ALPHABET[randIndex]);
+    }
+    return chars.join('');
+}
+
+var pageid = randId(128);
+
+new EventSource('/events/' + pageid).onmessage = function(e) { eval(e.data); }
+
+function call(method /*, elements*/) {
+    var data = [];
+    Array.prototype.slice.call(arguments, 1).forEach(function (elementId) {
+        data.push(get(elementId));
+    });
+
+    var r = new XMLHttpRequest();
+    r.open('POST', '/call/' + pageid, true);
+    r.setRequestHeader('Content-Type', 'application/json');
+    r.send(JSON.stringify({'method': method, 'params': data}));
+}
+
+function get(elementId) {
+    var element = document.getElementById(elementId);
+    if (element.value !== undefined) {
+        return element.value;
+    } else {
+        return element.innerHTML;
+    }
+}
+
+function set(elementId, value) {
+    var element = document.getElementById(elementId);
+    if (element.value !== undefined) {
+        element.value = value;
+    } else {
+        element.innerHTML = value;
+    }
+}
