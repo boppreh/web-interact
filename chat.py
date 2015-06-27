0aa212b519eefa107afcbd1c5b0fe012be432a26
BoppreH <boppreh@gmail.com>
2015-06-27 00:27:52 +0000

    Show name of all online users

--- chat.py
+++ chat.py
@@ -1,22 +1,31 @@
-from web import setup, PageBase, SessionBase
+from web import setup, PageBase, SessionBase, all_pages
 
 class Session(SessionBase):
     def __init__(self, id, pages):
         SessionBase.__init__(self, id, pages)
         self.name = ''
 
-    def change_name(self, new_name):
-        self.name = new_name
-        self.set('name', self.name)
-
 class Page(PageBase):
     def __init__(self, id, session):
         PageBase.__init__(self, id, session)
         self.set('name', session.name)
+        self._update_names()
+
+    def _update_names(self):
+        names = [s.session.name or 'Anon' for s in all_pages.values()]
+        self.set('users_online', ', '.join(names), 'world')
+
+    def change_name(self, new_name):
+        self.session.name = new_name
+        self.set('name', new_name)
+        self._update_names()
 
     def say(self, message):
         template = '<p><strong>{}</strong>: {}</p>'
         line = self.html(template, self.session.name or 'Anon', message)
         self.set('chat', self.get('chat') + line, 'world')
+
+    def __del__(self):
+        self._update_names()
 
 setup(Page, Session)
