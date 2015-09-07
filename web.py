6d5133102869bf7dba83973827b3a69c56ce5249
BoppreH <boppreh@gmail.com>
2015-09-07 22:03:59 +0000

    Add large JS and upload support

--- web.py
+++ web.py
@@ -3,13 +3,17 @@ import threading
 import re
 import json
 from collections import defaultdict
+from base64 import b64encode
 
 from html import escape as escape_html
 from urllib.parse import quote_plus
 
 class _Html(object):
     def __init__(self, template, values):
-        self.value = template.format(*map(escape_html, values))
+        if len(values):
+            self.value = template.format(*map(escape_html, values))
+        else:
+            self.value = template
 
     def __repr__(self):
         return repr(self.value)
@@ -29,11 +33,18 @@ class _Interactive(object):
     def eval(self, message, target=None):
         target = target or self.id
         if '\n' in message:
-            line = 'send {} eval(decodeURI("{}"))\n'.format(target, quote_plus(message))
+            line = 'send {} eval(atob("{}"))\n'.format(target, b64encode(message.encode('utf-8')).decode('ascii'))
         else:
             line = 'send {} {}\n'.format(target, message)
         _Interactive.socket_writer.write(line)
         _Interactive.socket_writer.flush()
+
+    def load_js(self, url):
+        self.eval("""
+var script = document.createElement('script');
+script.src = "{}";
+document.head.appendChild(script);
+""".format(url))
 
     def call(self, method, *args, target=None):
         # Python' and Javascript's quoting rules are close enough that we can
@@ -61,7 +72,7 @@ class PageBase(_Interactive):
         self.id = id
         self.session = session
 
-def setup(PageCls=PageBase, SessionCls=SessionBase, host='localhost', port=8001, auto_destroy_sessions=False):
+def setup(PageCls=PageBase, SessionCls=SessionBase, host='localhost', port=8001, upload=None, auto_destroy_sessions=False):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('localhost', 8001))
     reader = s.makefile('r', encoding='utf-8')
@@ -72,6 +83,7 @@ def setup(PageCls=PageBase, SessionCls=SessionBase, host='localhost', port=8001,
         while True:
             line = reader.readline()
             event, id, params = re.match(r'(\S+) (\S*) (.*)', line).groups()
+
             if event == 'connected':
                 session_id = params
                 if session_id in all_sessions:
@@ -85,6 +97,8 @@ def setup(PageCls=PageBase, SessionCls=SessionBase, host='localhost', port=8001,
                 all_pages[id] = page
                 page.on_open()
             elif event == 'disconnected':
+                if id not in all_pages:
+                    continue
                 session = all_pages[id].session
                 del session.pages[id]
                 del all_pages[id]
@@ -93,16 +107,24 @@ def setup(PageCls=PageBase, SessionCls=SessionBase, host='localhost', port=8001,
                     del all_sessions[session.id]
                     session.on_close()
             elif event == 'call':
+                if id not in all_pages:
+                    continue
                 call = json.loads(params)
                 method = call['method']
                 # Nope, not falling for that.
                 if method in dir(_Interactive) or method.startswith('_'):
-                    print('Somebody tried to call', method)
+                    print('Somebody tried to call and I blocked', method)
                     continue
                 try:
                     getattr(all_pages[id], method)(*call['params'])
                 except AttributeError:
-                    getattr(all_pages[id].session, method)(*call['params'])
+                    try:
+                        getattr(all_pages[id].session, method)(*call['params'])
+                    except AttributeError:
+                        print('Somebody tried to call, but theres no', method)
+            elif event == 'upload':
+                path, name = id, params
+                upload(path, name)
 
 
     threading.Thread(target=subroutine).start()
